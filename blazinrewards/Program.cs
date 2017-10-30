f7e543002b4ae2e15be97e14fa0b99b82f48fb06
Josh <NGUTustin@gmail.com>
2017-10-29 20:46:01 -0500

    remove useless includes

--- blazinrewards/Program.cs
+++ blazinrewards/Program.cs
@@ -4,14 +4,11 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
 using HtmlAgilityPack;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Threading;
-using System.Configuration;
 
 namespace blazinrewards
 {
